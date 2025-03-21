87b8798305c8ed6136853f3ff80ff6ce6ec1d61d
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-03-21 15:40:51 +0200

    implemented writing out to html file

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import os
 
-path_to_file = str(input("Enter the path to your md file: "))
-if (os.path.isfile(path_to_file)):
-    print("reading file...")
-    md_file = open(path_to_file, 'r')
-    for line in md_file:
-        print(line, end='')
+def main():
+    path_to_file = str(input("Enter the path to your md file: "))
+    if (os.path.isfile(path_to_file)):
+        if os.path.splitext(path_to_file)[1] != ".md":
+            print("Not a markdown file.")
+            return
+
+        print("reading file...")
+        md_file = open(path_to_file, "r")
+        html = ""
+
+        for line in md_file:
+            html += convert_to_html(line)
+
+        md_file.close()
+        html_file = open(os.path.splitext(md_file.name)[0] + ".html", "w")
+        html_file.write(html)
+        html_file.close()
+    else:
+        print("invalid path to file.")
+        return
+
+
+
+def convert_to_html(line):
+    return line
+
+if __name__ == "__main__":
+    main()
